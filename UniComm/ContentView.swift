19fd7ef9edb5dc4b4a216f7a39a86597b68c1420
Scriptwonder <51266340+Scriptwonder@users.noreply.github.com>
2020-02-11 11:55:18 -0500

    Initial Commit

---
+++ UniComm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UniComm
+//
+//  Created by 吴舒同 on 2020/2/11.
+//  Copyright © 2020 吴舒同. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
