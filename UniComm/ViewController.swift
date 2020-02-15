8938da58f6459cb76c392757601bdd85e1c1d8ac
Scriptwonder <51266340+Scriptwonder@users.noreply.github.com>
2020-02-15 18:49:06 -0500

    addObj

--- UniComm/ViewController.swift
+++ UniComm/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    //from login
+    @IBAction func username(_ sender: Any) {
+    }
+    @IBAction func password(_ sender: Any) {
+    }
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    @IBAction func register(_ sender: Any) {
+    }
+    //from register
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var reenter: UITextField!
+    
+    @IBAction func createAcc(_ sender: Any) {
+    }
+    
+    
+    //MARK: Actions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
