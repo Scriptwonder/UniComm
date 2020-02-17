18e7c439ecfd860c877f61bf330c1f39d96ba973
Scriptwonder <51266340+Scriptwonder@users.noreply.github.com>
2020-02-16 23:24:29 -0500

    change for 2/16

--- UniComm/Homepage.swift
+++ UniComm/Homepage.swift
@@ -14,6 +14,24 @@ struct Homepage: View {
     }
 }
 
+struct Films: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Books: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Music: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
 struct Homepage_Previews: PreviewProvider {
     static var previews: some View {
         Homepage()
