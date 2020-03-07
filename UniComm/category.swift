3b42a2e9df59bd7602a80ea02cf2786f09afc829
phabby <phabby92@gmail.com>
2020-03-07 17:53:53 -0500

    Bar and BarII work in preview, but they don't work in Homepage.

---
+++ UniComm/category.swift
@@ -0,0 +1,23 @@
+//
+//  category.swift
+//  UniComm
+//
+//  Created by 杨端 on 3/7/20.
+//  Copyright © 2020 吴舒同. All rights reserved.
+//
+
+import SwiftUI
+
+struct category: View {
+    var body: some View {
+        Text("This is Category")
+        //Bar()
+    }
+}
+
+struct category_Previews: PreviewProvider {
+    static var previews: some View {
+        category()
+        
+    }
+}
